61b8d3e29b5cd4cc0bd16c79ed1b93801713c783
edan4012 <91401797+edan4012@users.noreply.github.com>
2025-04-11 10:53:37 +0900

    Add files via upload

--- swipe_brick/swipe_brick/swipe_brick.c
+++ swipe_brick/swipe_brick/swipe_brick.c
@@ -24,6 +24,13 @@ typedef enum {
 
 int ball_speed = 5;
 
+void ResetBlocks(Rectangle blocks[MAX_BLOCKS], bool active[MAX_BLOCKS]) {
+    for (int i = 0; i < MAX_BLOCKS; i++) {
+        blocks[i] = (Rectangle){ 15 + (i % 25) * 63, 50 + (i / 25) * 30, 60, 20 };
+        active[i] = true;
+    }
+}
+
 int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Breakout Game"); //가로 세로 사이즈
     SetTargetFPS(60);
@@ -56,6 +63,9 @@ int main() {
             gamestate = STATE_PLAYING;
             score = 0;
             lifes = 5;
+            ball = (Vector2){ SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+            speed = (Vector2){ 5, -5 };
+            ResetBlocks(blocks, active);
         }
         if (gamestate == STATE_START) { //게임 시작화면 구성
             ClearBackground(RAYWHITE);
@@ -102,11 +112,16 @@ int main() {
         else if (gamestate == STATE_GAMEOVER) {
             //게임오버 화면
             ClearBackground(BLACK);
-            DrawText("GAME OVER...", SCREEN_WIDTH / 2 - 150, SCREEN_HEIGHT / 2, 40, RED);
+            DrawText("GAME OVER...", SCREEN_WIDTH / 2 - 180, SCREEN_HEIGHT / 2 - 30, 60, RED);
             DrawText("Press R to go Start Screen", SCREEN_WIDTH / 2 - 150, SCREEN_HEIGHT / 2 + 40, 20, WHITE);
 
             if (IsKeyPressed(KEY_R)) {
                 gamestate = STATE_START;
+                score = 0;
+                lifes = 5;
+                ball = (Vector2){ SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+                speed = (Vector2){ 5,-5 };
+                ResetBlocks(blocks, active);
             }
         }
         else if (gamestate == STATE_ENDING) {
@@ -116,6 +131,11 @@ int main() {
 
             if (IsKeyPressed(KEY_R)) {
                 gamestate = STATE_START;
+                score = 0;
+                lifes = 5;
+                ball = (Vector2){ SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+                speed = (Vector2){ 5,-5 };
+                ResetBlocks(blocks, active);
             }
         }
         else if (gamestate == STATE_PLAYING) {
